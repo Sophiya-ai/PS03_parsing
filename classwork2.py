82ce2028069df36d4c5d1a0d571b647d63595a5e
Sophiya-ai <sophiyabr@gmail.com>
2024-11-11 13:40:32 +0200

    cl2 fin

--- classwork2.py
+++ classwork2.py
@@ -6,19 +6,45 @@ def get_english_words():
     url = 'https://randomword.com/'
     try:
         res = req.get(url)
-        print(res.text)
+        #print(res.text)
         soup = BS(res.content,'html.parser')
 
         # Получаем слово. text.strip удаляет все пробелы из результата
-        e_words = soup.find_all('div', id='random_word')
+        e_word = soup.find('div', id='random_word').text.strip()
         # Получаем описание слова
-        w_def = soup.find_all('div', id='random_word_definition')
+        w_def = soup.find('div', id='random_word_definition').text.strip()
         # Чтобы программа возвращала словарь
         return {
-            "english_words" : e_words,
+            "english_word" : e_word,
             "english_word_definition" : w_def
         }
     except:
         print('Error!')
+
+# Создаём функцию, которая будет делать саму игру
+def word_game():
+    print("Добро пожаловать в игру")
+    while True:
+        word_dict = get_english_words()
+        word = word_dict.get("english_word")
+        word_definition = word_dict.get("english_word_definition")
+
+        # Начинаем игру
+        print(f"Значение слова - {word_definition}")
+        user = input("Что это за слово? ")
+        if user == word:
+            print("Все верно!")
+        else:
+            print(f"Ответ неверный, было загадано это слово - {word}")
+
+        # Создаём возможность закончить игру
+        play_again = input("Хотите сыграть еще раз? y/n")
+        if play_again != "y":
+            print("Спасибо за игру!")
+            break
+
+
+
+word_game()
 
     
